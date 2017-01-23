83f922cdd4a884d7243a2036db66386ed26ae2b9
Łukasz Sroczyński <Surreallistic@users.noreply.github.com>
2017-01-23 19:14:36 +0100

    Create encrptDecrypt.java

---
+++ JAVA/encrptDecrypt.java
@@ -0,0 +1,55 @@
+    public String encrypt(String valueToEncrypt) {
+        String encryptedValue = null;
+        try {
+            Key key = generateKey();
+            Cipher c = Cipher.getInstance(ALGORITHM);
+            c.init(Cipher.ENCRYPT_MODE, key);
+            byte[] encValue = c.doFinal(valueToEncrypt.getBytes());
+            encryptedValue = new BASE64Encoder().encode(encValue);
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedValue;
+    }
+
+    public String decrypt(String encryptedValue) {
+        String decryptedValue = null;
+        try {
+            Key key = generateKey();
+            Cipher c = Cipher.getInstance(ALGORITHM);
+            c.init(Cipher.DECRYPT_MODE, key);
+            byte[] decordedValue = new BASE64Decoder().decodeBuffer(encryptedValue);
+            byte[] decValue = c.doFinal(decordedValue);
+            decryptedValue = new String(decValue);
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return decryptedValue;
+    }
+
+    private static Key generateKey() throws Exception {
+        Key key = new SecretKeySpec(keyValue, ALGORITHM);
+        return key;
+    }
